8420d196bcef12d006622ac1f76c18587cf27121
Neyunse <36079551+Neyunse@users.noreply.github.com>
2021-12-19 16:50:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World xv');
-})
+app.set('views', __dirname + '/views'); // set files folder
+app.engine('html', require('ejs').renderFile); // render the html
+
+
+app.get('/', function (req, res){
+    res.render('index.html'); // show the render file
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
